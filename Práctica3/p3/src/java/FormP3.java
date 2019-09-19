ff62f86e88dcbbb25db9ed0399a5a48b5f9d603c
NeroLaw <54691146+NeroLaw@users.noreply.github.com>
2019-09-19 18:22:10 -0500

    Add files via upload

---
+++ Práctica3/p3/src/java/FormP3.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class FormP3 extends HttpServlet {
+ 
+   public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+      
+      // Set response content type
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      String title = "S1";
+      String docType =
+         "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+         
+      out.println("" +
+              docType +
+              "<html>\n" +
+                      "    <head>\n" +
+                      "        \n" +
+                      "        <title> Pr&aacute;ctica 3 </title>\n" +
+                      "       \n" +
+                      "    </head>\n" +
+                      "    <body>\n" +
+                      "        <form action=\"FormP3\" method=\"GET\" >\n" +
+                      "            Usuario: <input type=\"text\" name=\"u1\"/> <br> <br>\n" +
+                      "            Contrase&ntilde;a: <input type=\"password\" name=\"p1\"/> <br> <br>\n" +
+                      "            Correo: <input type=\"email\" name=\"e1\"/> <br> <br>\n" +
+                      "            Fecha de cumplea&ntilde;os: <input type=\"date\" name=\"d1\"/> <br> <br>\n" +
+                      "	<input type=\"submit\" name=\"s1\">\n" +
+                      "        </form>\n" +
+                      "    </body>\n" +
+                      "</html>\n"
+  
+      );
+    //throw  
+   }
+}
