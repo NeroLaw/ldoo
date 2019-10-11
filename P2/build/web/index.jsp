5a139fe37ce03c8801bb87e3e366978285474d91
NeroLaw <54691146+NeroLaw@users.noreply.github.com>
2019-10-10 20:29:08 -0500

    Práctica 2

---
+++ P2/build/web/index.jsp
@@ -0,0 +1,23 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title> Inicio </title>
+        
+    </head>
+    <body>
+        <label> Escribe lo que quieras para obterner tu ip y la hora </label>
+    
+    <form action = "index" method = "GET">
+        
+        <input type = "text" name = "all" placeholder="Lo que sea"/>
+                <br> <br>
+        <input type = "text" name = "All" placeholder="Igual"/>
+                <br><br>
+        <input type = "submit" value = "Submit" />
+      
+    </form>
+        
+    </body>
+</html>
---
+++ P2/src/java/Controlador/IpHora.java
@@ -0,0 +1,76 @@
+package Controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class IpHora extends HttpServlet {
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            String ip = null;
+            ip = request.getRemoteAddr();
+            Date date = new Date();
+            
+            
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title> Ip y Hora </title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<p>Tu IP: " + ip + "</p>");
+            out.println("<br> <br>");
+            out.println("<p>El tiempo: " + date.toString() + "</p>");
+            out.println("<br> <br>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
